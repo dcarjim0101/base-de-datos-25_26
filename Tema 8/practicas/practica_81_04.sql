fda6b75c55f4c289a071d4915379bbc6a605843d
dcarjim0101 <dcarjim0101@g.educaand.es>
2026-05-11 10:47:39 +0200

    clase_11_05

--- Tema 8/practicas/practica_81_04.sql
+++ Tema 8/practicas/practica_81_04.sql
@@ -14,3 +14,37 @@ CALL clientes_por_ciudad('Ubrique');
 -- ejercicio 2
 DELIMITER $$
 DROP PROCEDURE IF EXISTS movimientos_cuenta $$
+CREATE PROCEDURE movimientos_cuenta(IN p_cuenta VARCHAR(10))
+DROP DATABASE IF EXISTS movimientos_cuenta $$
+CREATE PROCEDURE movimientos WHERE cuenta_10 = p_cuenta ORDER BY FechaHora DESC;
+END $$
+
+-- uso del procedimiento
+DELIMITER ;
+CALL movimientos_cuenta(1);
+
+-- ejercicio 3
+DELIMITER $$
+DROP PROCEDURE IF EXISTS clientes_cuentas $$
+CREATE PROCEDURE clientes_cuentas(IN p_id_cliente INT UNSIGNED)
+BEGIN
+    SELECT * FROM cuentas WHERE id_cliente = p_id_cliente;
+END $$
+
+-- uso del procedimiento
+DELIMITER ;
+CALL clientes_cuentas(1);
+
+-- ejercicio 4
+DELIMITER $$
+DROP PROCEDURE IF EXISTS SaldosBajos $$
+CREATE PROCEDURE SaldosBajos()
+BEGIN
+    SELECT c.id_cuenta, c.iban, cl.nombre, cl.apellidos, cl.dni, c.saldo
+    FROM cuentas c
+    JOIN clientes cl ON c.id_cliente = cl.id_cliente
+    WHERE c.saldo <= 200;
+END $$
+DELIMITER ;
+-- uso del procedimiento
+CALL SaldosBajos();
